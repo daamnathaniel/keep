1ab8204a5dc43a794e812776f02f2c672ed57770
Nathaniel Adam <daamnathaniel@gmail.com>
2023-03-05 18:00:54 -0600

    another

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-  source /opt/asdf-vm/asdf.fish
+
+    source /opt/asdf-vm/asdf.fish
+
+    wal --theme random_dark
 
 
-  set -Ux EDITOR micro
-  set -Ux SHELL fish
 
-  
-  set -Ux kp '/usr/bin/git --git-dir=$HOME/.kp/ --work-tree=$HOME'
+    set -Ux EDITOR /usr/bin/micro
+    set -Ux BROWSER /usr/bin/brave
+    set -Ux QT_QPA_PLATFORM wayland
+    set -Ux QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+    set -Ux _JAVA_AWT_WM_NONREPARENTING 1
+
 
 
 end
+# alias kp='/usr/bin/git --git-dir=/home/nathaniel/.kp/.git/ --work-tree=/home/nathaniel'
